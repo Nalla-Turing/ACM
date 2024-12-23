5f4f0028c4e6eda364348c378b888166a37a3f22
Nalla-Turing <mrinaldhami2005@gmail.com>
2024-12-23 17:39:05 +0530

    Added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('ACM Login'),
-      centerTitle: true,
-      backgroundColor: ,
-    ),
-    body: Center(
-      child: Text('Add login details')
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Login'),
-    ),
-  ),
+  home: Home()
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ACM Login'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlue[600],
+      ),
+      body: Center(
+          child: Text(
+            'Add login details',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color: Colors.black,
+                fontFamily: 'Livvic'
+            ),
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: Text('Login button'),
+      ),
+    );
+  }
+}
