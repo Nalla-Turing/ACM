48caf4839bc0cc29d28751c51d2899ae0edf6226
Nalla-Turing <mrinaldhami2005@gmail.com>
2024-12-23 15:16:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('ACM Login'),
+      centerTitle: true,
+      backgroundColor: ,
+    ),
+    body: Center(
+      child: Text('Add login details')
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('Login'),
+    ),
+  ),
+));
